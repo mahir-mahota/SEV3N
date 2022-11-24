0b88d10839b15a20b27098ab414be0b07541fdc1
Mahir Mahota <mahirmahota@gmail.com>
2022-11-23 20:39:52 -0500

    Slightly re written code

---
+++ Rewritten test.c
@@ -0,0 +1,65 @@
+const int STRUMMING_POWER = 20;
+const int FRET_SPEED = -20;
+const int TIME_UNIT = 2000;
+const int FULL_ROTATION = 360;
+const int FRET_CLICKS = 30;
+const int FRET_TIMING = 1000;
+
+void pluck(int direction)
+{
+	motor[motorA] = direction * STRUMMING_POWER;
+
+	nMotorEncoder[motorA] = 0;
+
+	while(abs(nMotorEncoder[motorA]) < 90)
+	{}
+
+	motor[motorA] = 0;
+}
+
+void setAndPlayFret(int fret, int time, int direction)
+{
+	int clicksForFret = FRET_CLICKS * (fret - 1);
+	int clicksToRotate = FULL_ROTATION - clicksForFret;
+
+	time1[T1] = 0;
+
+	motor[motorD] = FRET_SPEED;
+	while(abs(nMotorEncoder[motorD]) % 360 < clicksToRotate)
+	{}
+	motor[motorD] = 0;
+
+	while(time1[T1] < hold * TIME_UNIT - FRET_TIMING)
+	{}
+
+	pluck(direction);
+
+	wait1Msec(FRET_TIMING);
+
+	motor[motorD] = FRET_SPEED;
+	while(abs(nMotorEncoder[motorD]) % 360 != 0)
+	{}
+	motor[motorD] = 0;
+}
+
+int playNotes(int *fret, int *hold, int length)
+{
+	int direction = 1;
+
+	for(int note = 0; note < length; note++)
+	{
+		setAndPlayFret(fret[note], hold[note], direction);
+
+		direction *= -1;
+	}
+	
+	return direction;
+}
+
+task main()
+{
+	int fret[10] = {2, 3, 5, 7, 10, 10, 7, 5, 3, 2};
+	int hold[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+	playNotes(fret, hold, 10);
+}
